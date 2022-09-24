6d92c80cfd8a34b92d65dc2790f776818badb042
Marcell Bolubás <pingvinmagas@gmail.com>
2022-09-24 21:30:32 +0200

    Refactor parts of Cloup::template_dir

--- src/utils.rs
+++ src/utils.rs
@@ -281,14 +281,12 @@ impl Cloup {
     fn template_dir() -> Result<PathBuf, String> {
         let config_dirname = dirs::data_dir().expect("Data dir not found").join("cloup");
 
-        if let Err(_) = fs::read_dir(&config_dirname) {
+        if fs::read_dir(&config_dirname).is_err() {
             return Err("Please run `cloup init` first in a template directory".to_string());
         }
 
-        let mut content = match fs::read_to_string(config_dirname.join(".config")) {
-            Ok(c) => c,
-            Err(_) => return Err("Config could not be parsed".to_string()),
-        };
+        let mut content = fs::read_to_string(config_dirname.join(".config")).expect("Config could not be read.");
+        
 
         if content.len() > 13 {
             content.replace_range(0..13, "");
